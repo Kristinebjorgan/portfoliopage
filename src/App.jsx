92f4af5aad1cd196fe33907b54dcfde9fc035c8e
Kristinebjorgan <kribjo53662@stud.noroff.no>
2025-06-05 18:06:43 +0100

    setup finally done, vite + react

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-offwhite text-green font-grotesk flex justify-center items-center text-4xl">
+      Tailwind is finally live.
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        green: "#296f5c",
+        gold: "#fdc751",
+        offwhite: "#fdf9ed",
+      },
+      fontFamily: {
+        grotesk: ['"Space Grotesk"', "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
